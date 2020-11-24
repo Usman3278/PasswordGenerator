feb1a22f0854e4ed27b45b6bf3323a2b9e7bda7f
Usman3278 <usman3278@gmail.com>
2020-11-24 20:29:47 +0500

    created new repo

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# passwordgenerator
+# Homework 03 JavaScript: Password Generator
+
+The purpose of this assignment was to create a secure random password generator. This secure password generator functions by prompting the user for the following criteria:
+
+Desired password length between a minimum of 8 characters and maximum of 128.
+
+Will the new password contain lowercase letters?
+Will the new password contain uppercase letters?
+Will the new password contain numbers?
+Will the new password contain special characters (!@#$%^&*)?
+
+Once the program has verified that the password length meets the minimum and maximum requirements then it determines which criteria the user has selected and generates a secured password based on the user's input. The new password is then displayed in the text area where it can copied. 
+
+This code was made it in JavaScript to create random password with Numbers, Special Character, Lowercase and Uppercase result.
+
+# Languages used to generate secured password are below.
+
+HTML
+CSS
+JS
+
+This code was made it in JavaScript to create random password with number, special character, lowercase and uppercase result.
+
+# Screenshots are uploaded into Assests Folder. 
---
+++ script.js
@@ -0,0 +1,79 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+//Generate password function
+
+function generatePassword() {
+
+//Define Variables
+// I used Let since this is New instead of Var
+
+let uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numbArray =      ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+//Array
+ 
+let resultArray = [];
+let userArray = [];
+
+uppercaseArray [1]
+//-----------------------------------------------------------------
+let numCharacter = prompt("How much number of Character you want between 8 and 128?");
+let numbers    = confirm ("Would you like numbers in your password?");
+let uppercases = confirm ("Would you like Uppercase characters in your password?");
+let lowercases = confirm ("Would you like Lowercase characters in your password?");
+let characters = confirm ("Would you like Special Characters in your password?");
+
+
+//User Input 
+
+if (numbers){
+  resultArray = resultArray.concat(numbArray);
+  console.log("numbers");
+  
+}
+
+if (uppercases){
+  resultArray = resultArray.concat(uppercaseArray);
+  console.log ("uppercases")
+
+}
+
+if (lowercases){
+  resultArray = resultArray.concat(lowercaseArray);
+  console.log ("lowercases")
+
+}
+
+if (characters){
+  resultArray = resultArray.concat(characterArray);
+  console.log ("characters")
+}
+
+  console.log(resultArray)
+
+
+for (let i = 0; i < numCharacter; i++) {
+      
+  userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
+  }
+
+  return userArray.join("") ;
+}
+
+// Write password to the #password input
+
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
